5eddd47e6d04bee7ac735a1a99cd5abaf7c7aef6
sachingangwar13 <sss97581300@gmail.com>
2025-08-14 13:22:54 +0530

    Frontend: remove vite.svg import breaking Vercel build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
